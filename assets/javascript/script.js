d0689a8b1f6c786f00244af07e2f6cc2e67c25e4
raywhui <rwhui.tech@gmail.com>
2017-09-02 03:59:12 -0700

    updated js with fully functional delete button and partially functional edit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,8 +12,12 @@ firebase.initializeApp(config);
 //create database var to access firebase
 var database = firebase.database();
 
-//btn on click event
-$('.btn').on('click',function(event){
+var num = 0;
+var dude = 0;
+
+//Submit btn on click event
+$('.btn').on('click', function(event){
+	console.log('hey now');
 	//prevent event from reloading page
 	//event.PreventDefault();
 
@@ -23,18 +27,27 @@ $('.btn').on('click',function(event){
 	var time = $('#first-train').val().trim();
 	var freq = $('#freq').val().trim();
 
+	//Conditional if nothing in input boxes
+	if (name === '' || dest === '' || time === '' || freq === ''){
+		alert('Nope');
+	}
+
+
+	else{
 	//object to hold new var values
-	var train = {
-		name:name,
-		dest:dest,
-		time:time,
-		freq:freq
+	function writeUserData(name,dest,time,freq){
+		database.ref().push({
+			trainname:name,
+			destination:dest,
+			arrivaltime:time,
+			frequency:freq
+		});
 	};
 
+	writeUserData(name,dest,time,freq);
 	//store object into firebase
 		//.push adds objects
 		//.set replaces objects
-	database.ref().push(train);
 
 	//emptys values
 	$('#name').val('');
@@ -42,50 +55,194 @@ $('.btn').on('click',function(event){
 	$('#first-train').val('');
 	$('#freq').val('');
 
+	};
 });
 
+//Adds entries from Firebase to DOM
 database.ref().on('child_added', function(snapshot){
-	console.log(snapshot.val());
-	console.log(snapshot.val().name);
-	console.log(snapshot.val().dest);
-	console.log(snapshot.val().time);
-	console.log(snapshot.val().freq);
-
-
 	//time conversion
-  var freq = snapshot.val().freq;
 
-	var time = snapshot.val().time;
+  var freq = snapshot.val().frequency;
 
+	var time = snapshot.val().arrivaltime;
+
+//setInterval(function(){
 	//Convert time. Pushed back one day to prevent negative difference
 	var timeConverted = moment(time,'HH:mm').subtract(1, "day");
+	var firstArrivalConverted = moment(timeConverted).format('hh:mm A');
 	console.log(timeConverted);
 
 	//Current time
 	var currentTime = moment();
-	console.log('Current Time: ' + moment().format('HH:mm'));
+	//console.log('Current Time: ' + moment().format('HH:mm'));
 
 	//Time difference in minutes from inital time to now
 	var timeDif = moment().diff(timeConverted, 'minutes');
-	console.log('Time Dif: ' + timeDif);
+	//console.log('Time Dif: ' + timeDif);
 
 	//Time frequency compared to time difference. Train arrival must occur when remainder is 0, so when freq - remainder gives time remaining.
 	var timeRemaining = freq - (timeDif % freq);
-	console.log('Time remaining: ' + timeRemaining);
+	//console.log('Time remaining: ' + timeRemaining);
 
 	//Til next train. Remainder + current time = next train arrival.
 	var nextTrain = moment().add(timeRemaining, 'm');
 	var nextTrainText = moment(nextTrain).format('hh:mm A');
 
-
 	//pull from firebase + add to table
-	$('.add-more-trains').append(
-		'<tr>' + 
-			'<td>' + snapshot.val().name + '</td>' + 
-			'<td>' + snapshot.val().dest + '</td>' + 
-			'<td>' + snapshot.val().freq + '</td>' + 
-			'<td>' + nextTrainText + '</td>' + 
-			'<td>' + timeRemaining +'</td>' + 
-		'</tr>')
+	var row = $('<tr class="del">');
+	var delBoxTd = $('<td>');
+	var delBoxLabel = $('<label><input type="checkbox" class="del-box"></label>');
+	
+	//Stores snapshots unique ID
+	var parentName = snapshot.key;
+	
+	//var newKey = database.ref().push().key; //gets unique parent ID of Firebase
+	console.log(parentName);
 
+	//appends Label to <td>
+	delBoxTd.append(delBoxLabel);
+
+	//Gives row attr of Unique ID
+	row.attr('data-uniqueID', parentName);
+	row.append(delBoxTd);
+	row.append(
+		'<td class="name">' + snapshot.val().trainname + '</td>' + 
+		'<td class="dest">' + snapshot.val().destination + '</td>' + 
+		'<td class="first-arrival">' + firstArrivalConverted + '</td>' +
+		'<td class="freq">' + snapshot.val().frequency + '</td>' + 
+		'<td class="arrival">' + nextTrainText + '</td>' + 
+		'<td class="time">' + timeRemaining +'</td>' +
+		'<td>' + '<div class="edit"></div>' + '</td>'
+	);
+
+	//appends row to DOM
+	$('.add-more-trains').append(row);
+	console.log('yay');
+	num++;
+
+	
+	// setInterval(function(){	
+	// 	$('.arrival-'+ dude).text(nextTrainText);
+	// 	$('.time-'+ dude).text(timeRemaining);
+	// 	console.log('ok cool');
+	// 	dude++;
+	// 	if (dude === num){
+	// 		dude = 0;
+	// 	}
+	// },3000);
 });
+
+//Delete Button
+$('.add-more-trains').on('click', '.del-box', function(){
+	//Grabs unique ID saved to attr. //.del-box>label>td>tr
+	var thisParent = $(this).parent().parent().parent().attr('data-uniqueID');
+
+	console.log(thisParent);
+
+	//Removes row from Firebase
+	database.ref(thisParent).remove();
+
+	//Removes row from DOM
+	$(this).parentsUntil('.add-more-trains').empty();
+});
+
+//Edit Button
+$('.add-more-trains').on('click', '.edit', function(){
+	console.log('sweg');
+	var thisParent = $(this).parentsUntil('.add-more-trains');
+
+	var initialName = thisParent.children('.name').text();
+	console.log(initialName);
+	var initialDest = thisParent.children('.dest').text();
+
+	//Convert Back to Military Time
+	var initialArrival = thisParent.children('.first-arrival').text();
+	var timeConverted = moment(initialArrival,'hh:mm A').subtract(1, "day");
+	var firstArrivalConverted = moment(timeConverted).format('HH:mm');
+
+	console.log(firstArrivalConverted)
+
+
+	var name = '.name';
+	var dest = '.dest';
+	var arrival = '.first-arrival';
+
+	function editRow(x,y,z){
+		thisParent.children(x).html('<input type="text" value="'+ y +'" class="'+ z +'">');
+	// $('.name');
+	// $('.dest').html('<input type="text">');
+	// $('.freq').html('<input type="text">');
+	};
+
+	editRow(name, initialName,'changedName');
+	editRow(dest, initialDest,'changedDest');
+	editRow(arrival, firstArrivalConverted,'changedArrival');
+
+	var newName = $('.changedName').val().trim();
+	var newDest = $('.changedDest').val().trim();
+	var newTime = $('.changedArrival').val().trim();
+	
+});
+
+
+	if (newName === '' || newDest === '' || newTime === ''){
+		alert('Invalid Entry');
+	}
+
+	else{
+
+	alert('yay');	
+	// 	function writeUserData(name,dest,time){
+	// 		database.ref().push({
+	// 			trainname:name,
+	// 			destination:dest,
+	// 			arrivaltime:time,
+	// 		});
+	// 	};
+
+	// 	writeUserData(newName,newDest,newTime);
+
+	 };
+
+
+
+//};
+
+
+
+//  	database.ref('train-schedule-'+ dude).on('value', function(snapshot){
+// 		//time conversion
+
+// 	  var freq = snapshot.val().frequency;
+// 		var time = snapshot.val().arrivaltime;
+
+// 	//setInterval(function(){
+// 		//Convert time. Pushed back one day to prevent negative difference
+// 		var timeConverted = moment(time,'HH:mm').subtract(1, "day");
+// 		//console.log(timeConverted);
+
+// 		//Current time
+// 		var currentTime = moment();
+// 		//console.log('Current Time: ' + moment().format('HH:mm'));
+
+// 		//Time difference in minutes from inital time to now
+// 		var timeDif = moment().diff(timeConverted, 'minutes');
+// 		//console.log('Time Dif: ' + timeDif);
+
+// 		//Time frequency compared to time difference. Train arrival must occur when remainder is 0, so when freq - remainder gives time remaining.
+// 		var timeRemaining = freq - (timeDif % freq);
+// 		//console.log('Time remaining: ' + timeRemaining);
+
+// 		//Til next train. Remainder + current time = next train arrival.
+// 		var nextTrain = moment().add(timeRemaining, 'm');
+// 		var nextTrainText = moment(nextTrain).format('hh:mm A');
+
+
+// 	});
+// },3000);
+
+
+
+
+
+//IF ITS ALWAYS CHECKING FIREBASE, ADD UPDATED TIME TO FIREBASE INSTEAD
