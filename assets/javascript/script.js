c5a24004f57202359467b421f9a639b9cfea1edf
raywhui <rwhui.tech@gmail.com>
2017-09-02 16:23:41 -0700

    completed edit funcitonality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -149,19 +149,17 @@ $('.add-more-trains').on('click', '.del-box', function(){
 //Edit Button
 $('.add-more-trains').on('click', '.edit', function(){
 	console.log('sweg');
+	//Goes up nodes right before .add-more-trains
 	var thisParent = $(this).parentsUntil('.add-more-trains');
 
+	//searches children with .name/.dest, takes text
 	var initialName = thisParent.children('.name').text();
-	console.log(initialName);
 	var initialDest = thisParent.children('.dest').text();
 
 	//Convert Back to Military Time
 	var initialArrival = thisParent.children('.first-arrival').text();
 	var timeConverted = moment(initialArrival,'hh:mm A').subtract(1, "day");
 	var firstArrivalConverted = moment(timeConverted).format('HH:mm');
-
-	console.log(firstArrivalConverted)
-
 
 	var name = '.name';
 	var dest = '.dest';
@@ -178,20 +176,110 @@ $('.add-more-trains').on('click', '.edit', function(){
 	editRow(dest, initialDest,'changedDest');
 	editRow(arrival, firstArrivalConverted,'changedArrival');
 
-	var newName = $('.changedName').val().trim();
-	var newDest = $('.changedDest').val().trim();
-	var newTime = $('.changedArrival').val().trim();
-	
+	var div = $('<div class="complete">');
+	var bar1 = '<div class="bar-one"></div>' + 
+							'<div class="bar-two"></div>';
+
+	div.append(bar1);
+	$(this).replaceWith(div);
+
+
 });
 
+//Complete after edit button
+$('.add-more-trains').on('click', '.complete', function(){
+	that = this;
+	var thisParent = $(this).parent().parent();
+	var thisUniqueId = thisParent.attr('data-uniqueID');
+	console.log(thisUniqueId);
 
-	if (newName === '' || newDest === '' || newTime === ''){
+	var newName = thisParent.find('.changedName').val();
+	var newDest = thisParent.find('.changedDest').val();
+
+	//Reconverts time back to hh:mm A
+	var newArrival = thisParent.find('.changedArrival').val();
+	var timeReconverted = moment(newArrival,'HH:mm').subtract(1, "day");
+	var firstArrivalReconverted = moment(timeReconverted).format('hh:mm A');
+
+
+		console.log(newName)
+		console.log(newDest)
+		console.log(firstArrivalReconverted)
+
+	if (newName === '' || newDest === '' || newArrival === ''){
 		alert('Invalid Entry');
 	}
 
 	else{
+		//updates Firebase 
+		function rewriteUserData(name,dest,time){
+			database.ref(thisUniqueId).update({
+				trainname:name,
+				destination:dest,
+				arrivaltime:time
+			});
+		};
 
-	alert('yay');	
+		//Replaces input with value into proper place
+		function emptyAndReplace(x,y){
+			thisParent.find(x).empty();
+			thisParent.find(x).append(y);
+		};
+
+		rewriteUserData(newName,newDest,newArrival);
+
+		emptyAndReplace('.name',newName);
+		emptyAndReplace('.dest',newDest);
+		emptyAndReplace('.first-arrival',firstArrivalReconverted);
+
+
+		//when child changed in Firebase, DO ALL THE TIME STUFF AGAIN
+		database.ref().on('child_changed', function(snapshot){
+	
+		//DO THE TIME STUFF ALL OVER AGAIN
+			var freq = snapshot.val().frequency;
+			var time = snapshot.val().arrivaltime;
+
+			//setInterval(function(){
+			//Convert time. Pushed back one day to prevent negative difference
+			var timeConverted = moment(time,'HH:mm').subtract(1, "day");
+			var firstArrivalConverted = moment(timeConverted).format('hh:mm A');
+			console.log(timeConverted);
+
+			//Current time
+			var currentTime = moment();
+			//console.log('Current Time: ' + moment().format('HH:mm'));
+
+			//Time difference in minutes from inital time to now
+			var timeDif = moment().diff(timeConverted, 'minutes');
+			//console.log('Time Dif: ' + timeDif);
+
+			//Time frequency compared to time difference. Train arrival must occur when remainder is 0, so when freq - remainder gives time remaining.
+			var timeRemaining = freq - (timeDif % freq);
+			//console.log('Time remaining: ' + timeRemaining);
+
+			//Til next train. Remainder + current time = next train arrival.
+			var nextTrain = moment().add(timeRemaining, 'm');
+			var nextTrainText = moment(nextTrain).format('hh:mm A');
+
+			//Pulls original this from .complete button
+			var thatParent = $(that).parent().parent();
+
+			//replace Next Arrival and Minutes Away
+			thatParent.find('.arrival').empty().append(nextTrainText);
+			thatParent.find('.time').empty().append(timeRemaining);
+		});	
+
+		//Replace + bar with - bar				
+		var bar = '<div class="edit"></div>'
+		$(this).replaceWith(bar);
+
+		// .prepend(
+		// '<td class="name">' + newName + '</td>' + 
+		// '<td class="dest">' + newDest + '</td>' + 
+		// '<td class="first-arrival">' + newTime + '</td>')
+
+	};
 	// 	function writeUserData(name,dest,time){
 	// 		database.ref().push({
 	// 			trainname:name,
@@ -202,7 +290,7 @@ $('.add-more-trains').on('click', '.edit', function(){
 
 	// 	writeUserData(newName,newDest,newTime);
 
-	 };
+});
 
 
 
